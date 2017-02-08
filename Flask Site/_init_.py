27929aeaf65bf8d7b0e83dbd2c1c8931cf312261
TFullerene <s1trojacek@yahoo.com>
2017-02-07 21:12:35 -0600

    Added Files and updated Html

--- Flask Site/_init_.py
+++ Flask Site/_init_.py
@@ -6,9 +6,23 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return render_template("main.html")
+
 @app.route("/about/")
 def about():
     return render_template("about.html")
+
+@app.route("/projects/")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/blog/")
+def blog():
+    return render_template("blog.html")
+    
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
